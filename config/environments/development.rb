4912303464e2042e056ccdaeb82c980487b6459f
byverbel_wsl <byverbel@gmail.com>
2021-11-12 01:17:13 -0500

    Added app security (SSL).

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,6 +80,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Mailcatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
 end
